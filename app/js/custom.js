1b489cf2497d3d22164cc7cd9b76fff3e8362655
Denis Maliyshock <maliy-shock@yandex.ru>
2017-02-16 21:28:15 +0300

    fix placeholder for email, fix mail type, fix ff box shadow on email, add active/not active to grid, change border color on grid button

--- app/js/custom.js
+++ app/js/custom.js
@@ -2,15 +2,19 @@ $('document').ready(function(){
     var formElements = {
         $formInput: $('.js-form-input'),
 
-        bindEvents: function () {
-            this.$formInput.on('blur', function () {
-              if( !$(this).val() ){
-                  $(this).removeClass('freeze')
-              }
-              else{
-                  $(this).addClass('freeze')
-              }
+        checkInput: function (obj) {
+            if( !obj.val() ){
+                obj.removeClass('freeze')
+            }
+            else{
+                obj.addClass('freeze')
+            }
+        },
 
+        bindEvents: function () {
+            formElements.checkInput( this.$formInput );
+            this.$formInput.on('blur', function () {
+                formElements.checkInput( $(this) );
             })
         }
     }
